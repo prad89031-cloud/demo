e238eea84e619bddbe1d28671189f0ba4de33660
Praveen Kumar <praveenkumar165516@gmail.com>
2025-09-28 19:05:23 +0530

    App

---
+++ Application/AccessRights/Departmentdropdown/DepartmentdropdownCommandHandler.cs
@@ -0,0 +1,25 @@
+﻿using System.Threading;
+using System.Threading.Tasks;
+using Core.AccessRights;
+using MediatR;
+
+namespace Application.AccessRights.Departmentdropdown
+{
+    
+    public class DepartmentdropdownCommandHandler : IRequestHandler<DepartmentdropdownCommand, object>
+    {
+        private readonly IAccessRightsRepository _repository;
+
+        public DepartmentdropdownCommandHandler(IAccessRightsRepository repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<object> Handle(DepartmentdropdownCommand command, CancellationToken cancellationToken)
+        {
+            
+            var result = await _repository.GetDepartmentsAsync(command.BranchId, command.OrgId);
+            return result;
+        }
+    }
+}
