d48c3badabde8041c32336a81faa00905495ef69
Praveen Kumar <praveenkumar165516@gmail.com>
2025-09-28 19:03:20 +0530

    app now

---
+++ Application/AccessRights/Departmentdropdown/DepartmentdropdownCommand.cs
@@ -0,0 +1,11 @@
+﻿using MediatR;
+
+namespace Application.AccessRights.Departmentdropdown
+{
+    
+    public class DepartmentdropdownCommand : IRequest<object>
+    {
+        public int BranchId { get; set; }
+        public int OrgId { get; set; }
+    }
+}
